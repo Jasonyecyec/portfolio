e6ab44ca14a184891d95f5095f388519b43dc8b4
Jasonyecyec <jasonyecyec@gmail.com>
2023-07-07 13:30:24 +0800

    fix 2

--- Portfolio/server.js
+++ Portfolio/server.js
@@ -22,10 +22,11 @@ const allowCors = (req, res, next) => {
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 
 const sendEmail = async ({ name, email, message }) => {
   try {
